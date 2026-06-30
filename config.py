83a99596b452117ab49ea83bede13435d8d8199e
sasacernysevskij-sys <sasacernysevskij@gmail.com>
2026-06-30 18:47:37 +0300

    real commit
    des

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-123")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///shop.db")
+SECRET_KEY = os.getenv("SECRET_KEY", "auth-secret-key-123")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///auth.db")
 JWT_EXPIRATION_HOURS = 24
-
-# SMTP настройки для отправки писем
-SMTP_HOST = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_EMAIL = "sasacernysevskij@gmail.com" 
-SMTP_PASSWORD = "qrgawqvxbiwobjfj"  
